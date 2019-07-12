3de83304f999d56efc7d4e60b0d38ffaa05fe059
lamb <1454040353@qq.com>
2019-07-12 15:22:43 +0800

    this is a first vue app test

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <common-header  bg="rgb(51,133,255)">
+            <span slot="title">Photo</span>
+        </common-header>
+        <common-footer  bg="rgb(51,133,255)"></common-footer>
+    </div>
+</template>
+
+<script>
+    import CommonHeader from "../common/Header.vue"
+    import CommonFooter from "../common/Footer.vue"
+    export default{
+        components: {
+            CommonHeader,
+            CommonFooter
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
